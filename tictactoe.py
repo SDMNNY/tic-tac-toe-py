181955486c86a9d292124b59c157f93d6649e04d
sdmnny <sdmnny@gmail.com>
2024-03-06 01:43:27 -0500

    created inital board and created first global variable

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,20 @@
+#first global variable
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+currentPlayer = "X"
+winner = None 
+gameRunning = True
+
+
+
 # printing the game board 
+def printBoard(board): 
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+    
 
 # take player input 
 
